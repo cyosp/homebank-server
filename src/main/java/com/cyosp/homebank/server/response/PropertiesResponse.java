270423a3d29adf577e3a3e0bc137592dcf3c0acd
CYOSP <cyosp@cyosp.com>
2019-04-22 15:02:27 +0200

    Add UT for properties response

--- src/main/java/com/cyosp/homebank/server/response/PropertiesResponse.java
+++ src/main/java/com/cyosp/homebank/server/response/PropertiesResponse.java
@@ -1,10 +1,8 @@
 package com.cyosp.homebank.server.response;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class PropertiesResponse {
 
     private String title;
--- src/test/java/com/cyosp/homebank/server/service/HomebankServiceTest.java
+++ src/test/java/com/cyosp/homebank/server/service/HomebankServiceTest.java
@@ -1,6 +1,8 @@
 package com.cyosp.homebank.server.service;
 
+import com.cyosp.homebank.server.repository.HomebankRepository;
 import com.cyosp.homebank.server.response.PaymentModeResponse;
+import com.cyosp.homebank.server.response.PropertiesResponse;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,23 +22,38 @@ import static org.junit.Assert.assertEquals;
 class HomebankServiceTest {
 
     @Autowired
+    private HomebankRepository homebankRepository;
+
     private HomebankService homebankService;
 
     private String resourceFolder;
 
+    private ObjectMapper objectMapper;
+
     @BeforeEach
     void setUp() {
+        homebankService = new HomebankService(homebankRepository);
         resourceFolder = getClass().getPackage().getName().replaceAll("\\.", "/");
+        objectMapper = new ObjectMapper();
     }
 
     @Test
     void paymentModes() throws IOException {
-
-        final List<PaymentModeResponse> paymentModes = new ObjectMapper().readValue(
+        final List<PaymentModeResponse> paymentModes = objectMapper.readValue(
                 getClass().getClassLoader().getResourceAsStream(resourceFolder + "/PaymentModeResponseList.json"),
                 new TypeReference<List<PaymentModeResponse>>() {
                 });
 
         assertEquals(paymentModes, homebankService.paymentModes());
+    }
+
+    @Test
+    void properties() throws IOException {
+        final PropertiesResponse propertiesResponse = objectMapper.readValue(
+                getClass().getClassLoader().getResourceAsStream(resourceFolder + "/PropertiesResponse.json"),
+                new TypeReference<PropertiesResponse>() {
+                });
+
+        assertEquals(propertiesResponse, homebankService.getProperties());
     }
 }
