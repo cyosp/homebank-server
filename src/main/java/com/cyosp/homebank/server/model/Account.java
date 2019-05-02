df90e9255984407f53fa97d840e69983d7460467
CYOSP <cyosp@cyosp.com>
2019-05-02 23:33:44 +0200

    Remove useless XStream alias

--- src/main/java/com/cyosp/homebank/server/model/Account.java
+++ src/main/java/com/cyosp/homebank/server/model/Account.java
@@ -1,6 +1,5 @@
 package com.cyosp.homebank.server.model;
 
-import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 import lombok.Data;
@@ -11,43 +10,33 @@ import java.math.BigDecimal;
 public class Account {
 
     @XStreamAsAttribute
-    @XStreamAlias("key")
     private Integer key;
 
     @XStreamAsAttribute
-    @XStreamAlias("flags")
     private Integer flags;
 
     @XStreamAsAttribute
-    @XStreamAlias("pos")
     private Integer pos;
 
     @XStreamAsAttribute
-    @XStreamAlias("type")
     private Integer type;
 
     @XStreamAsAttribute
-    @XStreamAlias("curr")
     private Integer curr;
 
     @XStreamAsAttribute
-    @XStreamAlias("name")
     private String name;
 
     @XStreamAsAttribute
-    @XStreamAlias("initial")
     private BigDecimal initial;
 
     @XStreamAsAttribute
-    @XStreamAlias("minimum")
     private BigDecimal minimum;
 
     @XStreamAsAttribute
-    @XStreamAlias("cheque1")
     private Long cheque1;
 
     @XStreamAsAttribute
-    @XStreamAlias("cheque2")
     private Long cheque2;
 
     @XStreamOmitField
