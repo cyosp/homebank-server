db396c73608a86ce084d9ec3362344f6df4fdded
CYOSP <cyosp@cyosp.com>
2019-05-06 22:52:47 +0200

    Improve code for Favorite class

--- src/main/java/com/cyosp/homebank/server/model/Favorite.java
+++ src/main/java/com/cyosp/homebank/server/model/Favorite.java
@@ -1,63 +1,47 @@
 package com.cyosp.homebank.server.model;
 
-import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
+@Data
 public class Favorite {
 
     @XStreamOmitField
     private Integer key;
 
-    //-----------------------------
-
     @XStreamAsAttribute
-    @XStreamAlias("amount")
     private BigDecimal amount;
 
     @XStreamAsAttribute
-    @XStreamAlias("account")
     private Integer account;
 
     @XStreamAsAttribute
-    @XStreamAlias("paymode")
     private Integer paymode;
 
     @XStreamAsAttribute
-    @XStreamAlias("flags")
     private Integer flags;
 
     @XStreamAsAttribute
-    @XStreamAlias("payee")
     private Integer payee;
 
     @XStreamAsAttribute
-    @XStreamAlias("category")
     private Integer category;
 
     @XStreamAsAttribute
-    @XStreamAlias("wording")
     private String wording;
 
     @XStreamAsAttribute
-    @XStreamAlias("nextdate")
     private Long nextdate;
 
     @XStreamAsAttribute
-    @XStreamAlias("every")
     private Integer every;
 
     @XStreamAsAttribute
-    @XStreamAlias("unit")
     private Integer unit;
 
     @XStreamAsAttribute
-    @XStreamAlias("limit")
     private Integer limit;
 }
