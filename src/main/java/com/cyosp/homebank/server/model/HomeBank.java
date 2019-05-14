7ae5901baa58415c59d84ba2d9cf7fb0a33a819e
CYOSP <cyosp@cyosp.com>
2019-05-14 22:57:31 +0200

    Improve code for Homebank class

--- src/main/java/com/cyosp/homebank/server/model/HomeBank.java
+++ src/main/java/com/cyosp/homebank/server/model/HomeBank.java
@@ -3,54 +3,40 @@ package com.cyosp.homebank.server.model;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 import com.thoughtworks.xstream.annotations.XStreamImplicit;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-import java.math.RoundingMode;
-import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 @XStreamAlias("homebank")
 public class HomeBank {
 
-    private static final int BIGDECIMAL_SCALE_0 = 0;
-    private static final int BIGDECIMAL_SCALE_2 = 2;
-    private static final RoundingMode BIGDECIMAL_ROUNDINGMODE = RoundingMode.HALF_UP;
-
-    @XStreamAlias("v")
     @XStreamAsAttribute
     private String v;
 
-    @XStreamAlias("d")
     @XStreamAsAttribute
     private String d;
 
-    @XStreamAlias("mpa")
-    private String mpa;
-
-    @XStreamAlias("properties")
     private Properties properties;
 
     @XStreamImplicit(itemFieldName = "cur")
-    private List<Currency> currencies = new ArrayList<>();
+    private List<Currency> currencies;
 
     @XStreamImplicit(itemFieldName = "account")
-    private List<Account> accounts = new ArrayList<>();
+    private List<Account> accounts;
 
     @XStreamImplicit(itemFieldName = "pay")
-    private List<Payee> payees = new ArrayList<>();
+    private List<Payee> payees;
 
     @XStreamImplicit(itemFieldName = "cat")
-    private List<Category> categories = new ArrayList<>();
+    private List<Category> categories;
 
     @XStreamImplicit(itemFieldName = "tag")
-    private List<Tag> tags = new ArrayList<>();
+    private List<Tag> tags;
 
     @XStreamImplicit(itemFieldName = "fav")
-    private List<Favorite> favorites = new ArrayList<>();
+    private List<Favorite> favorites;
 
     @XStreamImplicit(itemFieldName = "ope")
-    private List<Operation> operations = new ArrayList<>();
+    private List<Operation> operations;
 }
