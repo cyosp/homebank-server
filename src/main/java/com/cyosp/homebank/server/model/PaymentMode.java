b3ec9c962a052525321d104641b8ac296c933038
CYOSP <cyosp@cyosp.com>
2019-05-24 22:03:18 +0200

    Fix Bugs: Double Brace Initialization should not be used

--- src/main/java/com/cyosp/homebank/server/model/PaymentMode.java
+++ src/main/java/com/cyosp/homebank/server/model/PaymentMode.java
@@ -14,22 +14,22 @@ public class PaymentMode {
 
     public static final PaymentMode NO_PAYMENT_MODE_DEFINED = new PaymentMode(null, "", true);
 
-    public static final List<PaymentMode> PAYMENT_MODES = new ArrayList<PaymentMode>() {
-        {
-            add(NO_PAYMENT_MODE_DEFINED);
-            add(new PaymentMode(1, "Credit Card", true));
-            add(new PaymentMode(2, "Check", true));
-            add(new PaymentMode(3, "Cash", true));
-            add(new PaymentMode(4, "Transfer", false));
-            add(new PaymentMode(5, "Internal Transfer", false));
-            add(new PaymentMode(6, "Debit Card", true));
-            add(new PaymentMode(7, "Standing order", false));
-            add(new PaymentMode(8, "Electronic payment", false));
-            add(new PaymentMode(9, "Deposit", true));
-            add(new PaymentMode(10, "FI fee", false));
-            add(new PaymentMode(11, "Direct debit", false));
-        }
-    };
+    public static final List<PaymentMode> PAYMENT_MODES = new ArrayList<>();
+
+    static {
+        PAYMENT_MODES.add(NO_PAYMENT_MODE_DEFINED);
+        PAYMENT_MODES.add(new PaymentMode(1, "Credit Card", true));
+        PAYMENT_MODES.add(new PaymentMode(2, "Check", true));
+        PAYMENT_MODES.add(new PaymentMode(3, "Cash", true));
+        PAYMENT_MODES.add(new PaymentMode(4, "Transfer", false));
+        PAYMENT_MODES.add(new PaymentMode(5, "Internal Transfer", false));
+        PAYMENT_MODES.add(new PaymentMode(6, "Debit Card", true));
+        PAYMENT_MODES.add(new PaymentMode(7, "Standing order", false));
+        PAYMENT_MODES.add(new PaymentMode(8, "Electronic payment", false));
+        PAYMENT_MODES.add(new PaymentMode(9, "Deposit", true));
+        PAYMENT_MODES.add(new PaymentMode(10, "FI fee", false));
+        PAYMENT_MODES.add(new PaymentMode(11, "Direct debit", false));
+    }
 
     private Integer code;
 
